bd3224c51eb2809f2e4da271348b9a6e2109d1c4
imomarksasa <41761073+imomarksasa@users.noreply.github.com>
2018-09-21 00:41:36 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -36,7 +36,23 @@ client.on('ready',async () => {
   }
 });
 
+client.on('ready', function(){
+    var ms = 10000 ;
+    var setGame = [`Bot ON ${client.guilds.size} Servers`,`In Server ${client.users.size} Users`];
+    var i = -1;
+    var j = 0;
+    setInterval(function (){
+        if( i == -1 ){
+            j = 1;
+        }
+        if( i == (setGame.length)-1 ){
+            j = -1;
+        }
+        i = i+j;
+        client.user.setGame(setGame[i],`http://www.twitch.tv/KiNg66S`);
+    }, ms);
 
+});
 
 // THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);
